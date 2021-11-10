8f7fd12c42547ec64db93358f00ef7181cf433ff
Finn <flabe@physnet.uni-hamburg.de>
2021-11-10 11:29:01 +0100

    config file split

--- Validation/HLTrigger/python/ConfFile_client_cfg.py
+++ Validation/HLTrigger/python/ConfFile_client_cfg.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Demo")
+process = cms.Process("HLTGenValClient")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load("DQMServices.Core.DQM_cfg")
@@ -12,28 +12,26 @@ from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 ptBins=cms.vdouble(5,10,12.5,15,17.5,20,22.5,25,30,35,40,45,50,60,80,100,150,200,250,300,350,400)
 etaBins=cms.vdouble(-10, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 8, 10)
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/mc/RunIISummer20UL18RECO/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8/AODSIM/106X_upgrade2018_realistic_v11_L1v1-v2/00000/B4A06248-D09E-314A-ACD7-F157B86109E6.root")
-    #fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/relval/CMSSW_10_6_12/RelValTTbar_13UP16/AODSIM/PU25ns_106X_mcRun2_asymptotic_v13_hltul16_postVFP-v1/20000/67A99845-DA80-E048-BA70-CA5A4BF4F5E5.root")
+    processingMode = cms.untracked.string('RunsAndLumis'),
+    fileNames = cms.untracked.vstring("file:/nfs/dust/cms/user/flabe/HLTValidation/CMSSW_1200_HLTVal/src/Validation/HLTrigger/sourceoutput.root")
 )
 
-# the source
-process.HLTGenValSource = cms.EDProducer('HLTGenValSource',
+# the client
+process.HLTGenValClient = cms.EDProducer('HLTGenValClient',
 
     ### general configurations
-    DQMDirName = cms.string("HLTGenVal"),
-    genParticles = cms.InputTag("genParticles"),
+    InputDirName = cms.string("HLTGenVal_parts"),
+    OutputDirName = cms.string("HLTGenVal_effs"),
     hltProcessName = cms.string("HLT"), # this should be used to replace the following one if that one is needed
-    TrigEvent = cms.InputTag("hltTriggerSummaryAOD"),
     objType = cms.string("mu"),
 
     ### path configs
     # vector of paths that will be looked at. Need to get this automatically for certain objType later
     # not sure yet whether I need this, the list of filters, or both
     hltPathsToCheck = cms.vstring(
-      "HLT_IsoMu24_v",
       "HLT_Mu50_v",
     ),
 
@@ -55,14 +53,14 @@ process.HLTGenValSource = cms.EDProducer('HLTGenValSource',
 # the harvester
 process.harvester = DQMEDHarvester("DQMGenericClient",
     verbose        = cms.untracked.uint32(0), #set this to zero!
-    outputFileName= cms.untracked.string('testoutput.root'),
+    outputFileName= cms.untracked.string('clientoutput.root'),
     commands       = cms.vstring(),
     resolution     = cms.vstring(),
-    subDirs        = cms.untracked.vstring("HLTGenVal"),
-    efficiency     = cms.vstring("EFF 'title; label; label' ele_none_vspt ele_none_vspt")
+    subDirs        = cms.untracked.vstring("HLTGenVal_effs"),
+    efficiency     = cms.vstring("EFF 'title; label; label' mu_vspt_before mu_hltL1sSingleMu22or25_vspt_pass")
 )
 
-process.p = cms.Path(process.HLTGenValSource)
+process.p = cms.Path(process.HLTGenValClient)
 
 #process.dqmSaver.producer = 'HLTGenValSource'
 #process.dqmSaver.workflow = '/HLTGenValSource/HLTGenValHistColl/HLTGenValHist'
