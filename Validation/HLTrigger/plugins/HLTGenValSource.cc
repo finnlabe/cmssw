1f3afe0de6af48eddeda3178c7cdf97203f66dbf
Finn <flabe@physnet.uni-hamburg.de>
2021-11-24 17:05:07 +0100

    Added pset descriptions and defaults

--- Validation/HLTrigger/plugins/HLTGenValSource.cc
+++ Validation/HLTrigger/plugins/HLTGenValSource.cc
@@ -106,21 +106,10 @@ private:
 
 };
 
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-//
 HLTGenValSource::HLTGenValSource(const edm::ParameterSet& iConfig)
-    : genParticleToken_(consumes<reco::GenParticleCollection>(iConfig.getParameter<edm::InputTag>("genParticles"))),
-      genJetToken_(consumes<reco::GenJetCollection>(iConfig.getParameter<edm::InputTag>("genJets"))),
-      trigEventToken_(consumes<trigger::TriggerEvent>(iConfig.getParameter<edm::InputTag>("TrigEvent"))) {
+    : genParticleToken_( consumes<reco::GenParticleCollection>( iConfig.getParameterSet("inputCollections").getParameter<edm::InputTag>("genParticles") ) ),
+      genJetToken_(consumes<reco::GenJetCollection>( iConfig.getParameterSet("inputCollections").getParameter<edm::InputTag>("ak4GenJets"))),
+      trigEventToken_(consumes<trigger::TriggerEvent>( iConfig.getParameterSet("inputCollections").getParameter<edm::InputTag>("TrigEvent") )) {
 
       histConfigs_ = iConfig.getParameterSetVector("histConfigs");
 
@@ -202,10 +191,47 @@ void HLTGenValSource::bookHistograms(DQMStore::IBooker& iBooker, const edm::Run&
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void HLTGenValSource::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
   edm::ParameterSetDescription desc;
-  desc.setUnknown();
+
+  // basic parameter strings
+  desc.add<std::string>("objType"); // this deliberately has no default, as this is the main thing the user needs to chose
+  desc.add<std::vector<std::string>>("hltPathsToCheck"); // this for the moment also has no default: maybe there can be some way to handle this later?
+  desc.add<std::string>("DQMDirName", "HLTGenVal");
+  desc.add<std::string>("hltProcessName", "HLT");
+
+  // input collections, a PSet
+  edm::ParameterSetDescription inputCollections;
+  inputCollections.add<edm::InputTag>("genParticles", edm::InputTag("genParticles"));
+  inputCollections.add<edm::InputTag>("ak4GenJets", edm::InputTag("ak4GenJets"));
+  inputCollections.add<edm::InputTag>("ak8GenJets", edm::InputTag("ak8GenJets"));
+  inputCollections.add<edm::InputTag>("TrigEvent", edm::InputTag("hltTriggerSummaryAOD"));
+  desc.add<edm::ParameterSetDescription>("inputCollections", inputCollections);
+
+  // hist descriptors, which are a vector of PSets
+
+  // defining single histConfig
+  // this is generally without default, but a default set of histConfigs is specified below
+  edm::ParameterSetDescription histConfig;
+  histConfig.add<std::string>("vsVar");
+  histConfig.add<std::vector<double>>("binLowEdges");
+
+  // default set of histConfigs
+  std::vector<edm::ParameterSet> histConfigDefaults;
+
+  edm::ParameterSet histConfigDefault0;
+  histConfigDefault0.addParameter<std::string>("vsVar", "pt");
+  std::vector<double> defaultPtBinning{ 0,5,10,12.5,15,17.5,20,22.5,25,30,35,40,45,50,60,80,100,150,200,250,300,350,400 };
+  histConfigDefault0.addParameter<std::vector<double>>("binLowEdges", defaultPtBinning );
+  histConfigDefaults.push_back(histConfigDefault0);
+
+  edm::ParameterSet histConfigDefault1;
+  histConfigDefault1.addParameter<std::string>("vsVar", "eta");
+  std::vector<double> defaultetaBinning{ -10, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 8, 10 };
+  histConfigDefault1.addParameter<std::vector<double>>("binLowEdges", defaultetaBinning );
+  histConfigDefaults.push_back(histConfigDefault1);
+
+  desc.addVPSet("histConfigs", histConfig, histConfigDefaults);
+
   descriptions.addDefault(desc);
 }
 
--- Validation/HLTrigger/test/ConfFile_source_cfg.py
+++ Validation/HLTrigger/test/ConfFile_source_cfg.py
@@ -10,7 +10,7 @@ process.load("DQMServices.Components.MEtoEDMConverter_cff")
 from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 
 # parameter definitions
-ptBins=cms.vdouble(5,10,12.5,15,17.5,20,22.5,25,30,35,40,45,50,60,80,100,150,200,250,300,350,400)
+ptBins=cms.vdouble(0,5,10,12.5,15,17.5,20,22.5,25,30,35,40,45,50,60,80,100,150,200,250,300,350,400)
 etaBins=cms.vdouble(-10, -8, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 8, 10)
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
@@ -20,71 +20,15 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring("root://cmsxrootd.fnal.gov//store/relval/CMSSW_10_6_12/RelValTTbar_13UP16/AODSIM/PU25ns_106X_mcRun2_asymptotic_v13_hltul16_postVFP-v1/20000/67A99845-DA80-E048-BA70-CA5A4BF4F5E5.root")
 )
 
-# the sources
 process.HLTGenValSourceMU = cms.EDProducer('HLTGenValSource',
-
-    ### general configurations
-    DQMDirName = cms.string("HLTGenVal_parts"),
-    genParticles = cms.InputTag("genParticles"),
-    genJets = cms.InputTag("ak4GenJets"),
-    hltProcessName = cms.string("HLT"),
-    TrigEvent = cms.InputTag("hltTriggerSummaryAOD"),
+    # these are the only one the user needs to specify
     objType = cms.string("mu"),
-
-    ### path configs
-    # vector of paths that will be looked at. Need to get this automatically for certain objType later
-    # not sure yet whether I need this, the list of filters, or both
     hltPathsToCheck = cms.vstring(
       "HLT_IsoMu24_v",
     ),
-
-    ### histogram configs
-    # Configuring individual histograms, expecting vsVar as string and bin edges
-    # this histogram is created once for each filter specified above
-    histConfigs = cms.VPSet(
-        cms.PSet(
-            vsVar = cms.string("pt"),
-            binLowEdges = ptBins,
-        ),
-        cms.PSet(
-            vsVar = cms.string("eta"),
-            binLowEdges = etaBins,
-        ),
-    ),
-)
-process.HLTGenValSourceJET = cms.EDProducer('HLTGenValSource',
-
-    ### general configurations
-    DQMDirName = cms.string("HLTGenVal_parts"),
-    genParticles = cms.InputTag("genParticles"),
-    genJets = cms.InputTag("ak4GenJets"),
-    hltProcessName = cms.string("HLT"),
-    TrigEvent = cms.InputTag("hltTriggerSummaryAOD"),
-    objType = cms.string("jet"),
-
-    ### path configs
-    # vector of paths that will be looked at. Need to get this automatically for certain objType later
-    # not sure yet whether I need this, the list of filters, or both
-    hltPathsToCheck = cms.vstring(
-      "HLT_PFJet500_v",
-    ),
-
-    ### histogram configs
-    # Configuring individual histograms, expecting vsVar as string and bin edges
-    # this histogram is created once for each filter specified above
-    histConfigs = cms.VPSet(
-        cms.PSet(
-            vsVar = cms.string("pt"),
-            binLowEdges = ptBins,
-        ),
-        cms.PSet(
-            vsVar = cms.string("eta"),
-            binLowEdges = etaBins,
-        ),
-    ),
 )
 
-process.p = cms.Path(process.HLTGenValSourceJET*process.HLTGenValSourceMU)
+process.p = cms.Path(process.HLTGenValSourceMU)
 
 # the harvester
 process.harvester = DQMEDHarvester("HLTGenValClient",
